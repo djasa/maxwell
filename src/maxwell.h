4ba38ca89a6f36159c0e80f511999852e9fcf858
David Jaša <djasa@redhat.com>
2016-06-24 14:36:47 +0200

    Add GPLv2 headers to all files
    Solves https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=682035#12
    (along with already done LICENSE file inclusion).

--- src/maxwell.h
+++ src/maxwell.h
@@ -1,3 +1,24 @@
+/*
+ * Maxwell - A daemon to get entropy from timer randomness and feed it to
+ * 	random(4)
+ *
+ * Copyright (C) 2012-2015 Sandy Harris <sandyinchina@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
